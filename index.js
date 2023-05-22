d5c7ea11c048e3e7c525f327515b2ec8d55f1f24
dark7892 <dark78921@gmail.com>
2023-05-22 05:46:50 -0400

    add express

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
 const Web3 = require('web3');
-const MongoClient = require('mongodb').MongoClient;
 
-// MongoDB configuration
-const mongoURI = 'mongodb://localhost:27017'; // Replace with your MongoDB URI
+const app = express();
+const port = 3000; // Replace with your desired port number
+
+const mongoURI = 'mongodb+srv://haraki:<Bethebest123!>@cluster0.bgr1lex.mongodb.net/?retryWrites=true&w=majority'; // Replace with your MongoDB URI
 const dbName = 'bitbank'; // Replace with your database name
 const collectionName = 'exchange'; // Replace with your collection name
 
-// Ethereum network configuration
 const web3 = new Web3('https://erpc.apothem.network'); // Replace with your Infura project ID or Ethereum node URL
 const contractAddress = '0x2f78fc77fF3DfeFD469af6e21D2d1ad84216BC9c'; // Replace with your smart contract address
 const exchangeABI = require("./abi/bitbank.json"); // Replace with your smart contract ABI
 
-// Connect to MongoDB and Ethereum network
+app.get('/data', async (req, res) => {
+  try {
+    const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    await client.connect();
+
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+
+    const data = await collection.find().toArray();
+    res.json(data);
+
+    await client.close();
+  } catch (error) {
+    console.error('Error retrieving data:', error);
+    res.status(500).json({ error: 'Failed to retrieve data' });
+  }
+});
+
 async function initialize() {
   const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
   await client.connect();
@@ -31,7 +50,6 @@ async function initialize() {
   });
 }
 
-// Store event data in MongoDB
 async function storeEvent(collection, eventData) {
   try {
     await collection.insertOne(eventData);
@@ -41,5 +59,8 @@ async function storeEvent(collection, eventData) {
   }
 }
 
-// Start the event listener
 initialize().catch(console.error);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
