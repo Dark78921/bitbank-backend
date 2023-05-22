e8b1629c6cddd4fca1c623ddb3f8aef0907d36b5
dark7892 <dark78921@gmail.com>
2023-05-22 10:48:30 -0400

    addd ws to wss rpc url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const dbName = 'bitbank'; // Replace with your database name
 const collectionName = 'exchange'; // Replace with your collection name
 
 // const web3 = new Web3('https://erpc.apothem.network'); // Replace with your Infura project ID or Ethereum node URL
-const web3 = new Web3(new Web3.providers.WebsocketProvider('wss://ws.apothem.network'));
+const web3 = new Web3(new Web3.providers.WebsocketProvider('wss://ws.apothem.network/ws'));
 const contractAddress = '0x2f78fc77fF3DfeFD469af6e21D2d1ad84216BC9c'; // Replace with your smart contract address
 const exchangeABI = require("./abi/bitbank.json"); // Replace with your smart contract ABI
 
